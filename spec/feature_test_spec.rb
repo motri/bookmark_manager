e7bbc96937ba26bbe4eaa1250fd59ae7679143a6
Unai Motriko <unaimotriko@gmail.com>
2017-05-04 22:51:45 +0100

    It add user log in feature test

---
+++ spec/feature_test_spec.rb
@@ -0,0 +1,9 @@
+feature 'it signs into users link profile' do
+  scenario 'we sign in and takes us to link list' do
+    visit '/home'
+    fill_in :user, with: 'unaimotriko@electromail.org'
+    fill_in :password, with: 'doughnut'
+    click_on 'Log in'
+    expect(page).to have_content('Welcome back unaimotriko@electromail.org!')
+  end
+end
