5a11d7515ec50634be23783e8affeac2f549833c
Unai Motriko <unaimotriko@gmail.com>
2017-05-05 08:39:09 +0100

    Links

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,9 +13,7 @@ require_relative '../app'
 Capybara.app = BookmarkManager
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-                                                                 SimpleCov::Formatter::Console,
-                                                                 # Want a nice code coverage website? Uncomment this next line!
-                                                                 # SimpleCov::Formatter::HTMLFormatter
+SimpleCov::Formatter::Console, 
                                                                ])
 SimpleCov.start
 
