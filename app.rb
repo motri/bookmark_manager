48bb125f437d056f85efdbb7fe131007a9d07b7a
Unai Motriko <unaimotriko@gmail.com>
2017-05-05 15:54:04 +0100

    It adds heplpers

--- app.rb
+++ app.rb
@@ -40,4 +40,11 @@ class BookmarkManager < Sinatra::Base
     @links = tag ? tag.links : []
     erb :'links/index'
   end
+
+  helpers do
+   def current_user
+     @current_user ||= User.get(session[:user_id])
+   end
+  end
+
 end
