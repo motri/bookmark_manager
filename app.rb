701ca5f4ae809b1f5f8dc7ad0c4e75f35bd0256b
Unai Motriko <unaimotriko@gmail.com>
2017-05-05 16:00:37 +0100

    It defines current user and displays it on pages

--- app.rb
+++ app.rb
@@ -42,9 +42,8 @@ class BookmarkManager < Sinatra::Base
   end
 
   helpers do
-   def current_user
-     @current_user ||= User.get(session[:user_id])
-   end
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
   end
-
 end
